44b7ce7bc7aa38aa72eb3ebaeec39c753a60adde
tranthangadmin <49776697+tranthangadmin@users.noreply.github.com>
2019-11-02 02:24:56 +0700

    degin EditProfile

--- Task2BookingApp/app/src/main/java/com/example/bookingapp/ui/editprofile/EditProfile.java
+++ Task2BookingApp/app/src/main/java/com/example/bookingapp/ui/editprofile/EditProfile.java
@@ -1,5 +1,6 @@
 package com.example.bookingapp.ui.editprofile;
 
+import android.app.Activity;
 import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,21 +9,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.bookingapp.R;
+
+import java.util.ArrayList;
 
 public class EditProfile extends Fragment {
 
     private EditProfileViewModel mViewModel;
+    private Spinner spinnerDanhSach;
 
     public static EditProfile newInstance() {
         return new EditProfile();
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_edit_profile, container, false);
+        spinnerDanhSach = (Spinner) root.findViewById(R.id.spDoiTuong);
+        ArrayList<String> list =new ArrayList<>();
+        list.add("Công Ty");
+        list.add("Nhà trường");
+        ArrayAdapter adapter = new ArrayAdapter(getActivity(), R.layout.spinner_list_item,list);
+        spinnerDanhSach.setAdapter(adapter);
+        return root;
+
     }
 
     @Override
